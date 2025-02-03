e9c5fda39bb7412b11e60f372495851d8051f93e
Katerina Sklyarova <esidor27@mail.ru>
2025-02-03 12:46:59 +0300

    Удаление мусора

--- js/function.js
+++ js/function.js
@@ -2,32 +2,15 @@
 
 const checkLength = (str, maxLength) => str.length <= maxLength;
 
-// Строка короче 20 символов
-console.log(checkLength('nothing else matters', 20)); // true
-// Длина строки ровно 18 символов
-console.log(checkLength('korn', 4)); // true
-// Строка длиннее 10 символов
-console.log(checkLength('сектор газа', 10)); // false
+checkLength();
 
 
 //Функция для проверки, является ли строка палиндромом
 
 const isPalendrom = (str = '') => {
-  //Избавляюсь в строке от пробелов и привожу все символы в верхний регистр
   const normalize = str.replaceAll(' ', '').toUpperCase();
-
-  //Переворачиваю строку
   const reversed = normalize.split('').reverse().join('');
-
-  //Сравниваю входящую строку с перевернутой
   return normalize === reversed;
 };
 
-// Строка является палиндромом
-console.log(isPalendrom('топот')); // true
-// Несмотря на разный регистр, тоже палиндром
-console.log(isPalendrom('ДовОд')); // true
-// Это не палиндром
-console.log(isPalendrom('Кекс')); // false
-// Это палиндром
-console.log(isPalendrom('Лёша на полке клопа нашёл ')); // true
+isPalendrom();
