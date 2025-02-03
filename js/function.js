8b356e38dce562e350d007e81f8adec7a9a55e1e
Katerina Sklyarova <esidor27@mail.ru>
2025-02-03 10:10:36 +0300

    Добавляет функции из 2 задания

--- js/function.js
+++ js/function.js
@@ -1,16 +1,33 @@
-const getStringLength = (string = '', maxLength = 1) => string.length <= maxLength;
-getStringLength('проверяемая строка', 18);
+//Функция для проверки длины строки
 
-const isPalindrome = (string = '') => {
+const checkLength = (str, maxLength) => str.length <= maxLength;
 
-  string = string.replace(/\s+/g, '').toUpperCase();
+// Строка короче 20 символов
+console.log(checkLength('nothing else matters', 20)); // true
+// Длина строки ровно 18 символов
+console.log(checkLength('korn', 4)); // true
+// Строка длиннее 10 символов
+console.log(checkLength('сектор газа', 10)); // false
 
-  let newString = '';
 
-  for (let i = string.length - 1; i >= 0; i--) {
-    newString = newString + string[i];
+//Функция для проверки, является ли строка палиндромом
 
-  }
-  return string === newString;
+const isPalendrom = (str) => {
+  //Избавляюсь в строке от пробелов и привожу все символы в верхний регистр
+  const normalize = str.replaceAll(' ', '').toUpperCase();
+
+  //Переворачиваю строку
+  const reversed = normalize.split('').reverse().join('');
+
+  //Сравниваю входящую строку с перевернутой
+  return normalize === reversed;
 };
-isPalindrome('топот');
+
+// Строка является палиндромом
+console.log(isPalendrom('топот')); // true
+// Несмотря на разный регистр, тоже палиндром
+console.log(isPalendrom('ДовОд')); // true
+// Это не палиндром
+console.log(isPalendrom('Кекс')); // false
+// Это палиндром
+console.log(isPalendrom('Лёша на полке клопа нашёл ')); // true
