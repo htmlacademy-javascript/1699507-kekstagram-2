275d5da7247815943682b62dfb1605654561e0a0
Katerina Sklyarova <esidor27@mail.ru>
2025-02-08 09:16:10 +0300

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -11,6 +11,18 @@ const NAMES = [
   'Вашингтон',
 ];
 
+const DESCRIPTION = [
+  'Без фильтров',
+  'Новый вид животных',
+  'Жизнь в океане',
+  'Парад планет',
+  'Звездный дождь в феврале',
+];
+
+const COMMENTS = [
+  'Всё отлично!', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
 //Enam объекты
 const Likes = {
   MIN: 15,
@@ -22,20 +34,13 @@ const Comments = {
   MAX: 30,
 };
 
+const SvgCount = {
+  MIN: 1,
+  MAX: 6,
+};
+
 //Массив для заполнения данными
 const photos = [];
-
-const description = [
-  'Без фильтров',
-  'Новый вид животных',
-  'Жизнь в океане',
-  'Парад планет',
-  'Звездный дождь в феврале',
-];
-
-const comments = [
-  'Всё отлично!', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
 
 // Функция для получения случайного числа
 
@@ -58,9 +63,9 @@ const addComments = () => {
   for (let i = 0; i < getRandomInteger(Comments.MIN, Comments.MAX); i++) {
     feedback.push(
       {
-        id: getRandomInteger(0, 999),
-        avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
-        message: getRandomArrayElement(comments),
+        id: i,
+        avatar: `img/avatar-${ getRandomInteger(SvgCount.MIN, SvgCount.MAX) }.svg`,
+        message: getRandomArrayElement(COMMENTS),
         name: getRandomArrayElement(NAMES),
       }
     );
@@ -75,8 +80,8 @@ const addPhoto = () => {
   for (let i = 1; i <= PHOTO_COUNT; i++) {
     photos.push ({
       id: i,
-      url: `photos/${ i + 1 }.jpg`,
-      description: getRandomArrayElement(description),
+      url: `photos/${ i }.jpg`,
+      description: getRandomArrayElement(DESCRIPTION),
       likes: getRandomInteger(Likes.MIN, Likes.MAX),
       comments: addComments(),
     });
@@ -85,4 +90,4 @@ const addPhoto = () => {
   return photos;
 };
 
-addPhoto();
+console.log(addPhoto());
