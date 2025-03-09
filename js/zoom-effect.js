c7485f7b036b135ad6534a02d4bd658f9ee981d0
Katerina Sklyarova <esidor27@mail.ru>
2025-03-09 11:12:27 +0300

    Реализация зума

--- js/zoom-effect.js
+++ js/zoom-effect.js
@@ -1,7 +1,7 @@
 import { photoEditorForm } from './form-validation';
 
 //Шаг зума
-const zoom = {
+const ZoomStep = {
   MAX: 100,
   MIN: 25,
   STEP: 25,
@@ -15,3 +15,26 @@ const scaleValue = photoEditorForm.querySelector('.scale__control--value');
 //ищем картинку
 const imageThumbnail = photoEditorForm.querySelector('.img-upload__preview img');
 
+plusButton.addEventListener('click', () => {
+  let scale = parseInt(scaleValue.value, 10) + ZoomStep.STEP;
+
+  if (scale >= ZoomStep.MAX) {
+    scale = ZoomStep.MAX;
+  }
+
+  scaleValue.value = `${scale }%`;
+  scale = scale / 100;
+  imageThumbnail.style.transform = `scale(${ scale })`;
+});
+
+minusButton.addEventListener('click', () => {
+  let scale = parseInt(scaleValue.value, 10) - ZoomStep.STEP;
+
+  if (scale <= ZoomStep.MIN) {
+    scale = ZoomStep.MIN;
+  }
+
+  scaleValue.value = `${scale }%`;
+  scale = scale / 100;
+  imageThumbnail.style.transform = `scale(${ scale })`;
+});
