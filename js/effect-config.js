c4c05c0e90858d47c838e81d2611fdc3283c96b6
Katerina Sklyarova <esidor27@mail.ru>
2025-03-19 12:43:44 +0300

    Исправление

--- js/effect-config.js
+++ js/effect-config.js
@@ -1,5 +1,3 @@
-import { MAX_VALUE_SLIDER, MIN_VALUE_SLIDER } from './effect-slider';
-
 const STEP_COUNT_SLIDER = {
   stepMin: 0.1,
   stepMax: 1,
@@ -15,35 +13,35 @@ const FILTER_NAME = {
 
 const EffectConfig = {
   none: {
-    min: MIN_VALUE_SLIDER,
-    max: MAX_VALUE_SLIDER,
+    min: 0,
+    max: 100,
     step: STEP_COUNT_SLIDER.stepMax,
     unit: '',
   },
   chrome: {
     filter: FILTER_NAME.grayscale,
-    min: MIN_VALUE_SLIDER,
+    min: 0,
     max: STEP_COUNT_SLIDER.stepMax,
     step: STEP_COUNT_SLIDER.stepMin,
     unit: '',
   },
   sepia: {
     filter: FILTER_NAME.sepia,
-    min: MIN_VALUE_SLIDER,
+    min: 0,
     max: 1,
     step: STEP_COUNT_SLIDER.stepMin,
     unit: '',
   },
   marvin: {
     filter: FILTER_NAME.invert,
-    min: MIN_VALUE_SLIDER,
-    max: MAX_VALUE_SLIDER,
+    min: 0,
+    max: 100,
     step: STEP_COUNT_SLIDER.stepMax,
     unit: '%',
   },
   phobos: {
     filter: FILTER_NAME.blur,
-    min: MIN_VALUE_SLIDER,
+    min: 0,
     max: 3,
     step: STEP_COUNT_SLIDER.stepMin,
     unit: 'px',
